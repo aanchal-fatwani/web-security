3185ee333bad034d1b59a8fc538b6ae1b229762c
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-02-08 15:27:13 +0530

    Adding user-agent to bypass browser-only check

--- cors/server/getLinkedIn.js
+++ cors/server/getLinkedIn.js
@@ -1,7 +1,15 @@
 const axios = require("axios");
+let options = {
+  headers: {
+    "content-type": "application/json; charset=utf-8",
+    "Accept-Language": "fr",
+    "user-agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+  },
+};
 
 axios
-  .get("https://www.linkedin.com")
+  .get("https://www.linkedin.com", options)
   .then(function (response) {
     const getTitle = response?.data.match(/<title>(.*?)<\/title>/g);
     console.log(getTitle);
--- cors/server/getSwiggy.js
+++ cors/server/getSwiggy.js
@@ -1,7 +1,16 @@
 const axios = require("axios");
-
+let options = {
+  headers: {
+    "content-type": "application/json; charset=utf-8",
+    accept: "*/*",
+    "user-agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+  },
+};
 axios
-  .get("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.4723392&lng=80.3447932&page_type=DESKTOP_WEB_LISTING")
+  .get(
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.4723392&lng=80.3447932&page_type=DESKTOP_WEB_LISTING", options
+  )
   .then(function (response) {
     console.log(JSON.stringify(response?.data));
   })
